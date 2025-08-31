bc8943e7d8695fa8504a6678e65b78e3c75814f1
Albert Lionelle <albert.lionelle@colostate.edu>
2025-08-31 14:09:23 -0600

    Add example program demonstrating the use of program arguments in Rust

---
+++ rust/rustBasics/solutionCode/programArguments.rs
@@ -0,0 +1,22 @@
+///
+/// Example program showing how to use program arguments
+/// in rust.
+///
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    
+    println!("Number of arguments: {}", args.len());
+
+    for (i, arg) in args.iter().enumerate() {
+        println!("Argument {}: {}", i, arg);
+    }
+
+
+    if args.len() > 1 {
+        let first_arg = &args[0];
+        println!("The first argument is: {}", first_arg);
+        let second_arg = &args[1];
+        println!("The second argument is: {}", second_arg);
+    }
+}
